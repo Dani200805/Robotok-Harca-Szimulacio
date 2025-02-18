68f032a7c4cddb084f7f810e3c19c40d04daa966
Horvath Daniel <horvath.daniel1@student.jedlik.eu>
2025-02-18 18:18:32 +0100

    tovabbi feladatok elkeszitve

--- Robotok_Harca_Szimulacio/Robotok_Harca_Szimulacio/Program.cs
+++ Robotok_Harca_Szimulacio/Robotok_Harca_Szimulacio/Program.cs
@@ -31,13 +31,13 @@
                 switch (valasztas)
                 {
                     case "1":
-                        
+                        RobotAllapotMegtekintese();
                         break;
                     case "2":
-                        
+                        UjFegyverFelszerelese();
                         break;
                     case "3":
-                       
+                       UjFegyverHozzaadasa();
                         break;
                     case "4":
                         HarcInditasa();
@@ -63,6 +63,46 @@
             ellenfelek.Add(new Robot("Beta", 3, elerhetoFegyverek[1], new Pancel("Erős pajzs", 15)));
             ellenfelek.Add(new Robot("Gamma", 7, elerhetoFegyverek[2], new Pancel("Titan pajzs", 20)));
         }
+        static void RobotAllapotMegtekintese()
+        {
+            Console.WriteLine($"Robot neve: {jatekosRobot.Nev}, Szint: {jatekosRobot.Szint}, Fegyver: {jatekosRobot.Fegyver.Nev}, Páncél: {jatekosRobot.Pancel.Nev}");
+        }
+
+        static void UjFegyverFelszerelese()
+        {
+            Console.WriteLine("Válassz egy új fegyvert:");
+            for (int i = 0; i < elerhetoFegyverek.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {elerhetoFegyverek[i].Nev} - Sebzés: {elerhetoFegyverek[i].Sebzes}");
+            }
+            Console.Write("Választott fegyver száma: ");
+            if (int.TryParse(Console.ReadLine(), out int index) && index > 0 && index <= elerhetoFegyverek.Count)
+            {
+                jatekosRobot.Fegyver = elerhetoFegyverek[index - 1];
+                Console.WriteLine("Új fegyver felszerelve!");
+            }
+            else
+            {
+                Console.WriteLine("Érvénytelen választás.");
+            }
+        }
+
+        static void UjFegyverHozzaadasa()
+        {
+            Console.Write("Add meg az új fegyver nevét: ");
+            string nev = Console.ReadLine();
+            Console.Write("Add meg a fegyver sebzését: ");
+            if (int.TryParse(Console.ReadLine(), out int sebzes) && sebzes > 0)
+            {
+                elerhetoFegyverek.Add(new Fegyver(nev, sebzes));
+                Console.WriteLine("Új fegyver hozzáadva!");
+            }
+            else
+            {
+                Console.WriteLine("Érvénytelen sebzés érték.");
+            }
+        }
+
         static void HarcInditasa()
         {
             Console.WriteLine("Válassz egy ellenfelet:");
